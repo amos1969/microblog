aea4123d7f0b835a5fd8c51636b3088c2a6095ec
amos1969 <david.john.ames@gmail.com>
2026-06-22 17:09:14 +0100

    Code for Chapter 2 - added templates, dummy posts, and the beginnings of a menu system.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Eyup me ducks"
+    user = {'username': 'Dave'}
+    posts = [
+        {
+            'author': {'username': 'Connor'},
+            'body': 'Trier is hot today!'
+        },
+        {
+            'author': {'username': 'Karen'},
+            'body': 'Make sure you take a hat and water with you!'
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
